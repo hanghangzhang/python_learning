0158b541ae0911552837424ddfebb3608464bcaa
hanghangzhang <yzhang25@artic.edu>
2017-02-06 10:39:54 -0600

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+n=15
+r=[]
+for i in range(1,n+1):	
+	if i%15==0:
+		r.append("FizzBuzz")
+	elif i%3==0:		 		
+		r.append("Fizz")		
+	elif i%5==0:
+		r.append("Buzz") 						
+	else:
+		r.append(str(i))
+print r				
